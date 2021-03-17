d6b8489f91732d608def51c4610840bb948b98b6
Dennis Sheets <77841564+DennisSheets@users.noreply.github.com>
2021-03-17 10:41:55 -0500

    Update scripts.js
    moved the conditional logic from the event handler to the setSearchEngine function

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 
-function setSearchEngine(){
+function setSearchEngine() {
+            // let form = document.getElementById('searchForm'); //Jayde's
     let form = document.querySelector("form");
+            // let selectedEngine = document.querySelector('[name=engine]:checked'); //Jayde's
     let searchEngineSelected = document.querySelector('input[name=engine]:checked');
-    
+            // let searchTerm = document.querySelector("[name=q]").value; //Jayde's
+    let searchString = document.querySelector('input[name=q]');
+
     let searchEngines = {
         "google": "https://www.google.com/search",
         "duckduckgo": "https://duckduckgo.com/",
@@ -12,31 +16,26 @@ function setSearchEngine(){
         "baidu": "https://www.baidu.com/s"
     };
 
+            // if (searchTerm.trim() === "") {  //Jayde's
+    if(searchEngineSelected === null){
+        alert("Please select a search engine");
+        console.log(`searchEngineSelected: ${searchEngineSelected}`);
+        event.preventDefault();       // Why event. deprecated?  Alternative?
+             // return;   //Jayde's  I want EU to get both messages in once cycle if both are missing.
+    }
+    if (searchString.value === "") {
+        alert("Enter something in the search field");
+        console.log(`searchString: ${searchString.value}`);
+        event.preventDefault();       // Why event. deprecated?   Alternative?
+             // return   //Jayde's
+    }
+
     let searchEngineUrl = searchEngines[searchEngineSelected.value];
     form.setAttribute("action",searchEngineUrl);
-
 }
 
 window.addEventListener("load", function(){
     let form = document.getElementById("searchForm");
-
-        form.addEventListener("submit",function(event){
-
-            let searchEngineSelected = document.querySelector('input[name=engine]:checked');
-            let q = document.querySelector('input[name=q]');
-
-            if(searchEngineSelected === null){
-                alert("Please select a search engine");
-                console.log(`searchEngineSelected: ${searchEngineSelected}`);
-                event.preventDefault();
-            }
-
-            if (q.value === "") {
-                alert("Enter something in the search field");
-                console.log(`q: ${q.value}`);
-                event.preventDefault();
-            }
-            
-            form.addEventListener("submit",setSearchEngine);
-        });
+    form.addEventListener("submit",setSearchEngine);
 });
+
